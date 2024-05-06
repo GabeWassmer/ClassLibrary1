18c8f458f44d6b9f8117e4cf19c86cf28ba43dea
Gabe Wassmer <gwassmer@students.ccu.edu>
2024-05-06 04:36:02 -0600

    first commit

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please enter your birthday (yyyy/mm/dd):");
+        if (!DateTime.TryParse(Console.ReadLine(), out DateTime birthday))
+        {
+            Console.WriteLine("Invalid date format.");
+            return;
+        }
+
+        int ageInDays = (DateTime.Today - birthday).Days;
+        Console.WriteLine($"Your age in days is: {ageInDays}");
+    }
+}
